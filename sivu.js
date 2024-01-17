d64c87c7bc210a2d88084b800a3579fba9777db1
Joona Oinonen <Joona.oinonen@student.careeria.fi>
2024-01-17 11:03:20 +0200

    ruokkis

---
+++ sivu.js
@@ -0,0 +1,15 @@
+//Palvelut sivun näyttäminen
+
+function palvelut() {
+    let palvelut = `<h2> Palvelumme </h2>
+    <ul>
+
+    <li>Eläinlääkäri</li>
+    <li>Päivähoito</li>
+    <li>Yöhoito</li>
+    
+    
+    </ul>
+    `
+    document.getElementById("sisältö").innerHTML = palvelut
+}
